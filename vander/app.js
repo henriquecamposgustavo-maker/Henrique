4836570f989439a61b8d9d92f2488ae11df8d18b
henriquecamposgustavo-maker <henrique.campos.gustavo@escola.pr.gov.br>
2026-03-31 08:23:32 -0300

    Add files via upload

--- vander/app.js
+++ vander/app.js
@@ -1,4 +1,15 @@
-function calculoIMC() {
-    let imc = peso/(altura*altura);
+let inputTexto = document.querySelector(".texto");
 
+let idioma = document.querySelector(".idioma");
+
+let traducao =document.querySelector(".traducao");
+
+async function traduzir(){
+let url="https://api.mymemory.translated.net/get?q=Bom%20trabalho!&langpair=pt|en";
+
+let resposta = await fetch(url);
+
+let traducao = await resposta.json();
+
+console.log(traducao.responseData.translatedText);
 }
