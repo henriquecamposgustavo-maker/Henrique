61ddd9dc75a1239232b83059703d426731749065
henriquecamposgustavo-maker <henrique.campos.gustavo@escola.pr.gov.br>
2026-03-31 12:11:58 -0300

    Add files via upload

--- vander/app.js
+++ vander/app.js
@@ -2,14 +2,15 @@ let inputTexto = document.querySelector(".texto");
 
 let idioma = document.querySelector(".idioma");
 
-let traducao =document.querySelector(".traducao");
+let traducaoSaida =document.querySelector(".traducao");
 
 async function traduzir(){
-let url="https://api.mymemory.translated.net/get?q=Bom%20trabalho!&langpair=pt|en";
+let url="https://api.mymemory.translated.net/get?q=" + inputTexto.value + "&langpair=pt|" + idioma.value;
 
 let resposta = await fetch(url);
 
 let traducao = await resposta.json();
 
-console.log(traducao.responseData.translatedText);
+traducaoSaida.textContent=traducao.responseData.
+translatedText;
 }
